58e2a2a4dcba8c2184743497052bc078ce1b5ab5
Henry Xiang <hxxiang@ucdavis.edu>
2019-03-13 23:39:11 -0700

    created react app starter template

---
+++ start-react-app/precache-manifest.57d5ca894faee5f8a6805edb59e6edce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c5346e707b26350fc1494b2e0c94a1",
+    "url": "/static/media/light-bulb.29c5346e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8d7d1086db44b0b6d8c",
+    "url": "/static/js/main.169f7931.chunk.js"
+  },
+  {
+    "revision": "953d7ef086d27a6180f4",
+    "url": "/static/js/2.0a12b368.chunk.js"
+  },
+  {
+    "revision": "a8d7d1086db44b0b6d8c",
+    "url": "/static/css/main.376cb74d.chunk.css"
+  },
+  {
+    "revision": "e0192c39def90cad6a9dc6d0c654ea92",
+    "url": "/index.html"
+  }
+];

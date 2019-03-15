0fb5db488df2101daeedc08171f91638f2705880
Henry Xiang <hxxiang@ucdavis.edu>
2019-03-14 17:14:47 -0700

    rebuilt starter react app

---
+++ start-react-app/precache-manifest.b70179d2c9e5e4e3f377cae8080c6482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c5346e707b26350fc1494b2e0c94a1",
+    "url": "./static/media/light-bulb.29c5346e.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "896ec54bb84cea33c108",
+    "url": "./static/js/main.c3a0f291.chunk.js"
+  },
+  {
+    "revision": "10a62719862a56591f4d",
+    "url": "./static/js/2.22811df4.chunk.js"
+  },
+  {
+    "revision": "896ec54bb84cea33c108",
+    "url": "./static/css/main.ff11d6f1.chunk.css"
+  },
+  {
+    "revision": "10a62719862a56591f4d",
+    "url": "./static/css/2.bfc5bbc7.chunk.css"
+  },
+  {
+    "revision": "397f44fdf536cfc0fbcccff25c01e8de",
+    "url": "./index.html"
+  }
+];
--- start-react-app/service-worker.js
+++ start-react-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12f56ac5d7425a0209127d4b58faabfd.js"
+  "./precache-manifest.b70179d2c9e5e4e3f377cae8080c6482.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
